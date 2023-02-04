dd2258fce0504a411a049a0d7340b937a490b3fd
Rohan Bansal <bansalrohan18@gmail.com>
2023-02-04 14:41:20 -0800

    this is an actuallynice commit

--- testcams.py
+++ testcams.py
@@ -1,42 +1,19 @@
-from threading import Thread
-import cv2, time
- 
-class VideoStreamWidget(object):
-    def __init__(self, src=0):
-        self.capture = cv2.VideoCapture(src)
-        # Start the thread to read frames from the video stream
-        self.thread = Thread(target=self.update, args=())
-        self.thread.daemon = True
-        self.thread.start()
+import cv2
 
-    def update(self):
-        # Read the next frame from the stream in a different thread
-            while True:
-                if self.capture.isOpened():
-                    (self.status, self.frame) = self.capture.read()
-                    time.sleep(.01)
-    
-    def show_frame(self):
-        # Display frames in main program
-        cv2.imshow('frame', self.frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            self.capture.release()
-            cv2.destroyAllWindows()
-            exit(1)
+for i in range(5):
+    cv2.namedWindow('cam'+str(i))
+    cv2.moveWindow('cam'+str(i), 50*(i+1), 50*(i+1))
 
-if __name__ == '__main__':
-    video_stream_widget1 = VideoStreamWidget(src=9)
-    video_stream_widget2 = VideoStreamWidget(src=2)
-    video_stream_widget3 = VideoStreamWidget(src=5)
-    video_stream_widget4 = VideoStreamWidget(src=7)
-    video_stream_widget5 = VideoStreamWidget(src=11)
-    while True:
-        try:
-            video_stream_widget1.show_frame()
-            video_stream_widget2.show_frame()
-            video_stream_widget3.show_frame()
-            video_stream_widget4.show_frame()
-            video_stream_widget5.show_frame()
-        except AttributeError:
-            pass
+# capture from cameras
+cap = [cv2.VideoCapture(2), cv2.VideoCapture(5), cv2.VideoCapture(7), cv2.VideoCapture(9), cv2.VideoCapture(11)]
+
+while True:
+    for i in range(5):
+        ret, frame = cap[i].read()
+        cv2.imshow('cam'+str(i), frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+for i in range(5):
+    cap[i].release()
+cv2.destroyAllWindows()
